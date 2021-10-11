578d2aab5ce1e62af8594168b783a9ff68d57d06
nandoprado <pradolorenzo@gmail.com>
2021-10-11 12:12:15 +0200

    First data insertion

--- Restaurant.Services.ProductAPI/Migrations/20211011100318_SeedProducts.Designer.cs
+++ Restaurant.Services.ProductAPI/Migrations/20211011100318_SeedProducts.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Restaurant.Services.ProductAPI.DbContexts;
 
 namespace Restaurant.Services.ProductAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211011100318_SeedProducts")]
+    partial class SeedProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
